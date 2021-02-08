86e35fd2051f40d43c2b5a9d3d73ef1bf537a0c6
Safwan504 <safwanchowdhury7@gmail.com>
2021-02-09 01:02:32 +0600

    js file added

--- js/app.js
+++ js/app.js
@@ -1,7 +1,18 @@
 const searchMeal = () =>{
     const searchText = document.getElementById('search-field').value;
     const url = `https://www.themealdb.com/api/json/v1/1/filter.php?i=${searchText}`
+//    load data
     fetch(url)
     .then(res => res.json())
-    .then(data => console.log(data))
+    .then(data => displayMeals(data.meals))
+}
+
+const displayMeals = foods => {
+    const foodContainer = document.getElementById('food-container');
+
+    foods.forEach(food => {
+        const foodDiv = document.createElement('div');
+        foodDiv.innerText = food.strMeal;
+        foodContainer.appendChild(foodDiv);
+    });
 }
